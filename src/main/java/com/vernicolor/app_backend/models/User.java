2cb2e017630640779ad893e1113a95a4dd094795
kacem <kacemmeherzi52@gmail.com>
2024-09-07 12:18:17 +0100

    user model added

---
+++ src/main/java/com/vernicolor/app_backend/models/User.java
@@ -0,0 +1,25 @@
+package com.vernicolor.app_backend.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String role ;
+
+
+}
