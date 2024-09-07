b757cb51254473ea90f0645b00bc72a4b7c873ca
kacem <kacemmeherzi52@gmail.com>
2024-09-07 12:25:56 +0100

    product family DTO added

---
+++ src/main/java/com/vernicolor/app_backend/dto/ProdfamilyDTO.java
@@ -0,0 +1,13 @@
+package com.vernicolor.app_backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProdfamilyDTO {
+    private String name ;
+    private String description ;
+    private float quantity ;
+    private long  customerid ;
+}
