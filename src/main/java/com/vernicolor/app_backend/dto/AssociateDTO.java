dda24063af955f7c3711a6d61fd7bd79c7f0085c
kacem <kacemmeherzi52@gmail.com>
2024-09-07 12:36:48 +0100

    AssociateDTO between material and product family added

---
+++ src/main/java/com/vernicolor/app_backend/dto/AssociateDTO.java
@@ -0,0 +1,12 @@
+package com.vernicolor.app_backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class AssociateDTO {
+    private long prodfamid ;
+    private long matid ;
+}
