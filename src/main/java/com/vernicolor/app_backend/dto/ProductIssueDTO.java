55417dc6775096a156aed37710590d1c3dc241c8
kacem <kacemmeherzi52@gmail.com>
2024-09-07 16:30:31 +0100

    product issue DTO updated with attributes and getters and setters

--- src/main/java/com/vernicolor/app_backend/dto/ProductIssueDTO.java
+++ src/main/java/com/vernicolor/app_backend/dto/ProductIssueDTO.java
@@ -1,4 +1,16 @@
 package com.vernicolor.app_backend.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class ProductIssueDTO {
+    private long productId  ;
+     private String type ;
+    private String description ;
 }
