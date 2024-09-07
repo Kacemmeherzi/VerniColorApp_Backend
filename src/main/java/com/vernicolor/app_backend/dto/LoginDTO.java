6617e95c8ed24fc867bd3d36322dce01327599a2
kacem <kacemmeherzi52@gmail.com>
2024-09-07 12:21:47 +0100

    login dto added

---
+++ src/main/java/com/vernicolor/app_backend/dto/LoginDTO.java
@@ -0,0 +1,24 @@
+package com.vernicolor.app_backend.dto;
+
+public class    LoginDTO {
+    private String username;
+    private String password;
+
+    // Getters and Setters
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
