1b15481519d096211524ddf313e8e682d3659de5
kacem <kacemmeherzi52@gmail.com>
2024-09-08 18:01:44 +0100

    product model improved

--- src/main/java/com/vernicolor/app_backend/models/Product.java
+++ src/main/java/com/vernicolor/app_backend/models/Product.java
@@ -1,30 +1,29 @@
 package com.vernicolor.app_backend.models;
 
-import com.vernicolor.app_backend.models.ProductFamily;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
 
-import java.util.Date;
+
 import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
+
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id ;
     private String productName ;
     private String productDescription ;
-    private String productStatut ;
 
-    @Temporal(TemporalType.DATE)
-    private Date productValidatedAt ;
-    @Temporal(TemporalType.DATE)
-    private Date productCreatedAt ;
+    private String productStatus ;
+    private String productValidatedAt ;
+    private String productCreatedAt ;
     @ManyToOne
     @JoinColumn(name = "product_family_id", nullable = false)
     private ProductFamily productFamily;
