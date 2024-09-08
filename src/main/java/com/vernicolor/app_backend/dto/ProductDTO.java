0bedd137b2eeb0a9743ac29beb8ae77dc56757a0
kacem <kacemmeherzi52@gmail.com>
2024-09-08 18:05:31 +0100

    toString method and notNull annotation added for debugging reasons

--- src/main/java/com/vernicolor/app_backend/dto/ProductDTO.java
+++ src/main/java/com/vernicolor/app_backend/dto/ProductDTO.java
@@ -1,19 +1,22 @@
 package com.vernicolor.app_backend.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
-import java.util.Date;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@ToString
 public class ProductDTO {
     private long id;
     private String productName;
     private String productDescription;
-    private Date productValidatedAt;
+    @NotNull
+    private String productStatus ;
     private long productFamily;
 
 
